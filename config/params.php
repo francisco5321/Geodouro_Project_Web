dacc660f5190a423995a5aa1e43717d34c1cf43e
francisco5321 <frvitorino04fr@gmail.com>
2026-04-22 22:51:14 +0100

    fix

--- config/params.php
+++ config/params.php
@@ -5,7 +5,7 @@ return [
     'backendBaseUrl' => getenv('BACKEND_BASE_URL') ?: 'http://127.0.0.1:8080',
     'backendTimeoutSeconds' => (int) (getenv('BACKEND_TIMEOUT_SECONDS') ?: 8),
     'backendConnectTimeoutSeconds' => (int) (getenv('BACKEND_CONNECT_TIMEOUT_SECONDS') ?: 2),
-    'backendAuthTimeoutSeconds' => (int) (getenv('BACKEND_AUTH_TIMEOUT_SECONDS') ?: 3),
+    'backendAuthTimeoutSeconds' => (int) (getenv('BACKEND_AUTH_TIMEOUT_SECONDS') ?: 10),
     'backendAuthRequired' => filter_var(getenv('BACKEND_AUTH_REQUIRED') ?: false, FILTER_VALIDATE_BOOLEAN),
     'backendUploadsPath' => getenv('BACKEND_UPLOADS_PATH') ?: 'D:\\Universidade\\3 ANO\\2 SEMESTRE\\Estagio\\Geodouro_Project\\backend\\backend-uploads',
     'adminUsernames' => array_values(array_filter(array_map('trim', explode(',', getenv('ADMIN_USERNAMES') ?: '')))),
