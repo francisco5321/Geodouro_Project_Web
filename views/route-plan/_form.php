a5e1a468e090918c1c726f1d8235ea5579de9624
francisco5321 <frvitorino04fr@gmail.com>
2026-05-06 10:39:28 +0100

    fix btn

--- views/route-plan/_form.php
+++ views/route-plan/_form.php
@@ -12,7 +12,7 @@ use yii\bootstrap5\Html;
         <?= $form->field($model, 'description')->textarea(['rows' => 5, 'placeholder' => 'Define o objetivo deste percurso e as plantas/publicações que queres priorizar.']) ?>
 
         <div class="form-action-row">
-            <?= Html::submitButton($model->isNewRecord ? 'Criar percurso' : 'Guardar percurso', ['class' => 'btn btn-brand btn-lg']) ?>
+            <?= Html::submitButton($model->isNewRecord ? 'Criar percurso' : 'Confirmar', ['class' => 'btn btn-brand btn-lg']) ?>
             <a class="btn btn-outline-brand btn-lg" href="<?= yii\helpers\Url::to($model->isNewRecord ? ['route-plan/index'] : ['route-plan/view', 'id' => $model->route_plan_id]) ?>">Cancelar</a>
         </div>
     <?php ActiveForm::end(); ?>
